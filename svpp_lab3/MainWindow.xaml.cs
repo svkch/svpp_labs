32ae2c15979755a2db83f54ffee557aa422dc66f
Svetlana Kuchinskaya <s.a.kuchinskaya@gmail.com>
2024-05-26 23:42:45 +0300

    ver1.0

---
+++ svpp_lab3/MainWindow.xaml.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Color = System.Windows.Media.Color;
+using MessageBox = System.Windows.Forms.MessageBox;
+using Path = System.IO.Path;
+using Point = System.Windows.Point;
+using SaveFileDialog = Microsoft.Win32.SaveFileDialog;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+
+namespace svpp_lab3
+{
+
+    public partial class MainWindow : Window
+    {
+        private Color bck_color = Colors.Turquoise;
+        private Color ln_color = Colors.Black;
+        public double ln_thickness = 0.5;
+
+        private List<Shape> figures = new List<Shape>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DrawHexagon(object sender, MouseButtonEventArgs e)
+        {
+            Point position = e.GetPosition(DrawingCanvas);
+
+            status.Text = $" Координаты фигуры:     x: {position.X},   y: {position.Y}";
+
+            double side = 50;
+            double height = side * Math.Sqrt(3) / 2;
+
+            Polygon polygon = new Polygon();
+
+            polygon.Points = new PointCollection()
+            {
+
+                new Point(position.X - side, position.Y - height / 2),
+                new Point(position.X - side / 2, position.Y - height),
+                new Point(position.X + side / 2, position.Y - height),
+
+                new Point(position.X + side, position.Y - height / 2),
+
+                new Point(position.X + side / 2 , position.Y + height / 16),
+                new Point(position.X - side / 2, position.Y + height / 16) 
+            };
+
+            polygon.Stroke = new SolidColorBrush(ln_color);
+            polygon.StrokeThickness = slider_thickness.Value;
+            polygon.StrokeThickness = ln_thickness;
+            polygon.Fill = new SolidColorBrush(bck_color);
+
+            DrawingCanvas.Children.Add(polygon);
+            figures.Add(polygon);
+        }
+
+        private void thick_slider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            foreach (var child in DrawingCanvas.Children)
+            {
+                if (child is Polygon)
+                {
+                    ((Polygon)child).StrokeThickness = slider_thickness.Value;
+                }
+            }
+        }
+
+        private void chng_clr_back_Click(object sender, RoutedEventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                bck_color = Color.FromArgb(colorDialog.Color.A, colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+                SolidColorBrush brush = new SolidColorBrush(bck_color);
+            }
+
+        }
+
+        private void chng_clr_border_Click(object sender, RoutedEventArgs e)
+        {
+            ColorDialog colorDialog = new ColorDialog();
+            if (colorDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                ln_color = Color.FromArgb(colorDialog.Color.A, colorDialog.Color.R, colorDialog.Color.G, colorDialog.Color.B);
+                SolidColorBrush brush = new SolidColorBrush(ln_color);
+            }
+        }
+
+        private void ln_thick_menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            ThicknessDialog thicknessDialog = new ThicknessDialog();
+            if (thicknessDialog.ShowDialog() == true)
+            {
+                ln_thickness = thicknessDialog.Thickness;
+            }
+        }
+
+        private void about_menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Название программы: Графический редактор\nВерсия: 1.0\nАвтор: Кучинская С.А.");
+        }
+
+        private Color StringToColor(string colorString)
+        {
+            var colorParts = colorString.Split(',');
+            byte a = byte.Parse(colorParts[0], CultureInfo.InvariantCulture);
+            byte r = byte.Parse(colorParts[1], CultureInfo.InvariantCulture);
+            byte g = byte.Parse(colorParts[2], CultureInfo.InvariantCulture);
+            byte b = byte.Parse(colorParts[3], CultureInfo.InvariantCulture);
+            return Color.FromArgb(a, r, g, b);
+        }
+
+        private void save_to_file(string filePath)
+        {
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                foreach (var figure in figures)
+                {
+                    if (figure is Polygon polygon)
+                    {
+                        writer.WriteLine("Информация о фигуре (фигурах)");
+                        writer.WriteLine(polygon.StrokeThickness);
+                        writer.WriteLine($"{((SolidColorBrush)polygon.Stroke).Color.A},{((SolidColorBrush)polygon.Stroke).Color.R},{((SolidColorBrush)polygon.Stroke).Color.G},{((SolidColorBrush)polygon.Stroke).Color.B}");
+                        writer.WriteLine($"{((SolidColorBrush)polygon.Fill).Color.A},{((SolidColorBrush)polygon.Fill).Color.R},{((SolidColorBrush)polygon.Fill).Color.G},{((SolidColorBrush)polygon.Fill).Color.B}");
+
+                        foreach (var point in polygon.Points)
+                        {
+                            writer.WriteLine($"{point.X},{point.Y}");
+                        }
+
+                        writer.WriteLine("Конец информации");
+                    }
+                }
+            }
+        }
+
+        private void load_from_file(string filePath)
+        {
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line == "Информация о фигуре (фигурах)")
+                    {
+                        Polygon polygon = new Polygon
+                        {
+                            StrokeThickness = double.Parse(reader.ReadLine(), CultureInfo.InvariantCulture),
+                            Stroke = new SolidColorBrush(StringToColor(reader.ReadLine())),
+                            Fill = new SolidColorBrush(StringToColor(reader.ReadLine()))
+                        };
+
+                        var points = new PointCollection();
+                        while ((line = reader.ReadLine()) != "Конец информации")
+                        {
+                            var pointParts = line.Split(',');
+                            double x = double.Parse(pointParts[0], CultureInfo.InvariantCulture);
+                            double y = double.Parse(pointParts[1], CultureInfo.InvariantCulture);
+                            points.Add(new Point(x, y));
+                        }
+
+                        polygon.Points = points;
+                        DrawingCanvas.Children.Add(polygon);
+                        figures.Add(polygon);
+                    }
+                }
+            }
+        }
+
+        private void save_menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            if (figures.Count > 0)
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog
+                {
+                    Filter = "Text Files|*.txt"
+                };
+
+                if (saveFileDialog.ShowDialog() == true)
+                {
+                    save_to_file(saveFileDialog.FileName);
+                    this.Title = $"Graphics Editor - {Path.GetFileName(saveFileDialog.FileName)}";
+                }
+            }
+            else
+            {
+                System.Windows.MessageBox.Show("Нет фигур для сохранения!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void open_menuItem_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Text Files|*.txt"
+            };
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                load_from_file(openFileDialog.FileName);
+                this.Title = $"Graphics Editor - {Path.GetFileName(openFileDialog.FileName)}";
+            }
+        }
+    }
+}
---
+++ svpp_lab3/ThicknessDialog.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace svpp_lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для ThicknessDialog.xaml
+    /// </summary>
+
+    public partial class ThicknessDialog : Window
+    {
+        public double Thickness { get;  set; }
+
+        public ThicknessDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void ok_button_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ThicknessTextBox.Text, out double thickness) && thickness > 0)
+            {
+                Thickness = thickness;
+                DialogResult = true;
+                Close();
+            }
+            else
+            {
+                MessageBox.Show("Введите корректное значение толщины.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void cancel_button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            Close();
+        }
+    }
+}
+    
